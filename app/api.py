133e65653daecb7e19a4eddc694899acd6a10453
Josué Juca <josuejuca19@gmail.com>
2024-02-09 18:08:40 -0300

    [09/02/2024] - API QrCode | Vercel

---
+++ app/api.py
@@ -0,0 +1,30 @@
+import io
+import qrcode
+
+from fastapi import FastAPI, Query
+from starlette.responses import StreamingResponse
+
+app = FastAPI()
+
+@app.get('/')
+def home():
+    return {"API": "JUCA Soft"}
+
+@app.get("/qrcode/{texto}")
+def QrAPI(texto: str, size: int = Query(default=200, description="Tamanho do QR Code")):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(texto)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="black", back_color="white")
+    img = img.resize((size, size))
+
+    buf = io.BytesIO()
+    img.save(buf, format="PNG")
+    buf.seek(0)
+    return StreamingResponse(buf, media_type="image/png")
