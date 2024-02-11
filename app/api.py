906da72cae78f923f866db66449aec1df44ce71d
Josué Juca <josuejuca19@gmail.com>
2024-02-10 22:46:36 -0300

    [10/02/2024] - Atualizando rota da API ✨

--- app/api.py
+++ app/api.py
@@ -32,3 +32,24 @@ def QrAPI(texto: str, size: int = Query(default=200, description="Tamanho do QR 
     img.save(buf, format="PNG")
     buf.seek(0)
     return StreamingResponse(buf, media_type="image/png")
+
+
+
+@app.get("/data")
+def QRAPI(text: str = Query(..., description="Conteúdo do QR Code"), size: int = Query(default=200, description="Tamanho do QR Code")):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(text)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="black", back_color="white")
+    img = img.resize((size, size))
+
+    buf = io.BytesIO()
+    img.save(buf, format="PNG")
+    buf.seek(0)
+    return StreamingResponse(buf, media_type="image/png")
